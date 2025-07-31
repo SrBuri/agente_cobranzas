0079efb9e44f21d14dcbecf62c03c53a6a0cb533
SrBuriCundi <jmburitica@ucundinamarca.edu.co>
2025-07-31 13:52:51 -0500

    se agrega el archivo config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+def cargar_flujo(path="flujo.txt"):
+    with open(path, "r", encoding="utf-8") as f:
+        return f.read()
+
+def get_model():
+    return ChatOpenAI(
+        model="gpt-4.1-mini-2025-04-14",
+        temperature=0.3,
+        openai_api_key=os.getenv("OPENAI_API_KEY")
+    )
